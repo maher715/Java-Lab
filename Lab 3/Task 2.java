6a696d01ad969b871dfab67caef6b9beadffa181
maher715 <e243017@ugrad.iiuc.ac.bd>
2025-06-30 23:19:02 +0600

    Update Task 2.java

--- Lab 3/Task 2.java
+++ Lab 3/Task 2.java
@@ -1,25 +1,32 @@
-
-import java.util.Scanner; import
-java.util.ArrayList;
-class Main{ public static void main(String[] args) {
-Scanner chkPrime = new Scanner(System.in);
-System.out.print("Enter the starting number: "); int start
-= chkPrime.nextInt();
-System.out.print("Enter the ending number: "); int end
-= chkPrime.nextInt();
-ArrayList<Integer> primeNumbers = new ArrayList<>(); for
-(int i = start; i <= end; i++) { if
-(isPrime(i)) {
-primeNumbers.add(i);
+import java.util.Scanner;
+public class PrimeNumbersArray {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter Starting Number: ");
+int start = sc.nextInt();
+System.out.print("Enter Ending Number: ");
+int end = sc.nextInt();
+int[] primeNumbers = new int[end - start];
+int index = 0;
+for (int i = start; i <= end; i++) {
+if (isPrime(i)) {
+primeNumbers[index] = i;
+index++;
 }
 }
-Integer[] primeArray = primeNumbers.toArray(new Integer[0]);
-System.out.println("Prime numbers between " + start + " and " + end + ":"); for
-(int num : primeArray) { System.out.print(num + " ");
+System.out.println("Prime Numbers in the Given Range:");
+for (int i = 0; i < index; i++) {
+System.out.print(primeNumbers[i] + " ");
 }
-chkPrime.close();
-} public static boolean isPrime(int num) { if (num <= 1) { return false; } for (int i = 2; i <= Math.sqrt(num); i++)
-{ if (num % i == 0) { return false;
-} } return true;
+sc.close();
+}
+public static boolean isPrime(int num) {
+if (num < 2) return false;
+for (int i = 2; i <= num / 2; i++) {
+if (num % i == 0) {
+return false;
+}
+}
+return true;
 }
 }
