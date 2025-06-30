2a26e9d9828b199164f1fbb8cd731a66f684cf6c
maher715 <e243017@ugrad.iiuc.ac.bd>
2025-06-30 23:07:00 +0600

    Create Task 2.java

---
+++ Lab 1/Task 2.java
@@ -0,0 +1,11 @@
+class Main
+{
+public static void main(String[] args) {
+String name = "Muhammad Nasrullah Hoque Maher";
+String studentId = "E243017";
+String email = "mhoquechy786@gmail.com";
+String number = "01645939786";
+String address = "Hathazari";
+System.out.println("Name: " + name +"\nStudent Id: "+ studentId + "\nEmail: "+email +"\nPhone: "+ number +"\nAddress: "+ address);
+}
+}
